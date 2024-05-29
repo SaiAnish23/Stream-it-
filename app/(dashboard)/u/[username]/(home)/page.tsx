b7a3590ddc12478daa971b4f06df878185f2b1b8
SaiAnish23 <anish231003@gmail.com>
2024-05-29 23:28:13 +0530

    Add StreamPlayer component for displaying user streams

--- app/(dashboard)/u/[username]/(home)/page.tsx
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,5 +1,8 @@
 import { currentUser } from "@clerk/nextjs";
 
+import { getUserByUsername } from "@/lib/user-service";
+import {StreamPlayer}  from "@/components/stream-player";
+
 interface CreatorPageProps {
   params: {
     username: string;
@@ -7,7 +10,19 @@ interface CreatorPageProps {
 }
 
 const CreatorPage = async ({ params }: CreatorPageProps) => {
-  return <div className="h-full">CReator Page</div>;
+  const externalUser = await currentUser();
+  const user = await getUserByUsername(params.username);
+
+  if (!user || user.externalUserId !== externalUser?.id  || !user.stream) {
+    throw new Error("Unauthorized");
+  }
+
+  return (
+    <div className="h-full">
+      <StreamPlayer user={user} stream={user.stream} isFollowing />
+      User Creater page
+    </div>
+  );
 };
 
 export default CreatorPage;
