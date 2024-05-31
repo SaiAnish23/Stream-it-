c4159e836d004e9af8409922f3426ee8a738948c
SaiAnish23 <anish231003@gmail.com>
2024-05-31 17:43:50 +0530

    feat: Add UserLoading and NotFoundPage components

---
+++ app/(browse)/[username]/loading.tsx
@@ -0,0 +1,12 @@
+// import { StreamPlayerSkeleton } from "@/components/stream-player";
+
+const UserLoading = () => {
+  return (
+    <div className="h-full">
+      {/* <StreamP layerSkeleton /> */}
+      Loading....
+    </div>
+  );
+};
+
+export default UserLoading;
